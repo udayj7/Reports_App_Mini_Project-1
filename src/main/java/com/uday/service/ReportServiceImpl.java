675ff7a0047857aa90d00f30d8dcd7f2d2ac8ba4
Udayj5521 <udayj4597@gmail.com>
2023-10-04 00:37:27 +0530

    attached pdf and excel in email

--- src/main/java/com/uday/service/ReportServiceImpl.java
+++ src/main/java/com/uday/service/ReportServiceImpl.java
@@ -1,5 +1,6 @@
 package com.uday.service;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
@@ -34,12 +35,24 @@ import com.lowagie.text.pdf.PdfWriter;
 import com.uday.entity.CitizenPlan;
 import com.uday.repo.CitizenPlanRepository;
 import com.uday.request.SearchRequest;
+import com.uday.util.EmailUtils;
+import com.uday.util.ExcelGenerator;
+import com.uday.util.PdfGenerator;
 
 @Service
 public class ReportServiceImpl implements ReportService {
 
 	@Autowired
 	private CitizenPlanRepository planRepo;
+
+	@Autowired
+	private ExcelGenerator excelGenerator;
+
+	@Autowired
+	private PdfGenerator pdfGenerator;
+
+	@Autowired
+	private EmailUtils emailUtils;
 
 	@Override
 	public List<String> getPlanName() {
@@ -90,93 +103,32 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public boolean exportExcel(HttpServletResponse response) throws Exception {
 
-//		Workbook workbook = new XSSFWorkbook();
-		Workbook workbook = new HSSFWorkbook();
-		Sheet sheet = workbook.createSheet("plans-data");
-		Row headerRow = sheet.createRow(0);
+		File f = new File("Plans.xls");
 
-		headerRow.createCell(0).setCellValue("ID");
-		headerRow.createCell(1).setCellValue("Citizen Name");
-		headerRow.createCell(2).setCellValue("Plan Name");
-		headerRow.createCell(3).setCellValue("Plan Status");
-		headerRow.createCell(4).setCellValue("Plan Start Date");
-		headerRow.createCell(5).setCellValue("Plan End Date");
-		headerRow.createCell(6).setCellValue("Benefit Amt");
+		List<CitizenPlan> plans = planRepo.findAll();
+		excelGenerator.generate(response, plans, f);
 
-		List<CitizenPlan> records = planRepo.findAll();
+		String subject = "Mini-Project-JRTP";
+		String body = "<h1>Reports Project file Attached Excel File</h1>";
+		String to = "udayj5521@gmail.com";
 
-		int dataRowIndex = 1;
-
-		for (CitizenPlan plan : records) {
-			Row dataRow = sheet.createRow(dataRowIndex);
-			dataRow.createCell(0).setCellValue(plan.getCitizenId());
-			dataRow.createCell(1).setCellValue(plan.getCitizenName());
-			dataRow.createCell(2).setCellValue(plan.getPlanName());
-			dataRow.createCell(3).setCellValue(plan.getPlanStatus());
-
-			if (null != plan.getPlanStartDate()) {
-				dataRow.createCell(4).setCellValue(plan.getPlanStartDate() + "");
-			} else {
-				dataRow.createCell(4).setCellValue("N/A");
-			}
-			if (null != plan.getPlanEndDate()) {
-				dataRow.createCell(5).setCellValue(plan.getPlanEndDate() + "");
-			} else {
-				dataRow.createCell(5).setCellValue("N/A");
-			}
-			if (null != plan.getBenefitAmt()) {
-				dataRow.createCell(6).setCellValue(plan.getBenefitAmt());
-			} else {
-				dataRow.createCell(6).setCellValue("N/A");
-			}
-			dataRowIndex++;
-		}
-		ServletOutputStream outputStream = response.getOutputStream();
-		workbook.write(outputStream);
-		workbook.close();
+		emailUtils.sendEmail(subject, body, to, f);
+		f.delete();
 		return true;
 	}
 
 	@Override
 	public boolean pdfExport(HttpServletResponse response) throws Exception {
-
-		Document document = new Document(PageSize.A4);
-
-		PdfWriter.getInstance(document, response.getOutputStream());
-
-		document.open();
-
-		Font fontTiltle = FontFactory.getFont(FontFactory.TIMES_ROMAN);
-		fontTiltle.setSize(20);
-
-		Paragraph p = new Paragraph("Citizen Plans Info", fontTiltle);
-		p.setAlignment(Paragraph.ALIGN_CENTER);
-		document.add(p);
-
-		PdfPTable table = new PdfPTable(6);
-		table.setSpacingBefore(5);
-
-		table.addCell("Id");
-		table.addCell("Citizen Name");
-		table.addCell("Plan Name");
-		table.addCell("Plan Status");
-		table.addCell("Start Date");
-		table.addCell("End Date");
-
+		File f = new File("Plans.pdf");
 		List<CitizenPlan> plans = planRepo.findAll();
+		pdfGenerator.generate(response, plans, f);
 
-		for (CitizenPlan plan : plans) {
-			table.addCell(String.valueOf(plan.getCitizenId()));
-			table.addCell(plan.getCitizenName());
-			table.addCell(plan.getPlanName());
-			table.addCell(plan.getPlanStatus());
-			table.addCell(plan.getPlanStartDate() + "");
-			table.addCell(plan.getPlanEndDate() + "");
-		}
+		String subject = "Mini-Project-JRTP";
+		String body = "<h1>Reports Project file Attached PDF File</h1>";
+		String to = "udayj5521@gmail.com";
 
-		document.add(table);
-		document.close();
-
+		emailUtils.sendEmail(subject, body, to, f);
+		f.delete();
 		return false;
 	}
 
--- src/main/java/com/uday/util/EmailUtils.java
+++ src/main/java/com/uday/util/EmailUtils.java
@@ -1,5 +1,33 @@
 package com.uday.util;
 
+import java.io.File;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailUtils {
 
+	@Autowired
+	private JavaMailSender mailSender;
+
+	public boolean sendEmail(String subject, String body, String to, File f) {
+
+		try {
+			MimeMessage createMimeMessage = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(createMimeMessage, true);
+			helper.setSubject(subject);
+			helper.setText(body, true);
+			helper.setTo(to);
+			helper.addAttachment("Plans-Info", f);
+			mailSender.send(createMimeMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
 }
